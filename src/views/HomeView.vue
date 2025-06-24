6e31062096d12fb8f2d64ef73e4581c5273650d6
ayi <17325218783@163.com>
2025-06-24 15:00:54 +0800

    清理模板

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex min-h-screen flex-col items-center justify-center">
+    <h1 class="text-4xl font-bold">我的 Vue IT 工具箱</h1>
+    <p class="mt-4 text-lg">即将在这里添加更多强大的工具！</p>
   </main>
 </template>
