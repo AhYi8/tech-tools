052c32b4bbc0252c05c0761999ab2c85c229bb04
ayi <17325218783@163.com>
2025-06-26 10:44:50 +0800

    v1.0.0

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <main class="flex min-h-screen flex-col items-center justify-center">
-    <h1 class="text-4xl font-bold">我的 Vue IT 工具箱</h1>
-    <p class="mt-4 text-lg">即将在这里添加更多强大的工具！</p>
-  </main>
-</template>
